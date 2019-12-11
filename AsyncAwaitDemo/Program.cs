c7b35ba98a0da61ede50e19e49b383edd8c24776
wturi <bolizhazi@live.cn>
2019-12-11 11:24:37 +0800

    update

---
+++ AsyncAwaitDemo/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncAwaitDemo
+{
+    internal class Program
+    {
+        private static SemaphoreSlim _semaphore = new SemaphoreSlim(1);
+
+        private static Dictionary<string, object> _demo = new Dictionary<string, object>();
+
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                GoGoGo("a" + i, "a" + i, "a" + i);
+            }
+            for (int i = 1; i < 6; i++)
+            {
+                var a = GoGoGo("a" + i, "a" + i, "a" + i);
+            }
+
+            Console.ReadLine();
+        }
+
+
+
+        private static async Task<string> GoGoGo(string arg1, string arg2, string arg3)
+        {
+
+            return await Task.Run(() =>
+            {
+                Console.WriteLine(arg1 + "开始排队...");
+                Console.WriteLine(arg1 + "开始执行...");
+                Get(arg1, arg1);
+                Console.WriteLine();
+                Console.WriteLine(arg1 + "执行完毕，离开！");
+                return "结束";
+            });
+        }
+
+
+        public static object Get(string name, object obj)
+        {
+            _semaphore.Wait();
+            Thread.Sleep(1000);
+            if (_demo.ContainsKey(name))
+            {
+
+                _semaphore.Release();
+                return _demo[name];
+            }
+            else
+            {
+                _demo.Add(name, obj);
+                _semaphore.Release();
+                return obj;
+            }
+        }
+    }
+}
