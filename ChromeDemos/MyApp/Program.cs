c144a96585870610a1f7fc697c8a4e94da3a050d
wturi <bolizhazi@live.cn>
2019-12-18 17:22:58 +0800

    Add ChromeNativeMessage Demo

---
+++ ChromeDemos/MyApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFileExtension ="config", Watch = true)]
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public static log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                string chromeMessage = OpenStandardStreamIn();
+
+                log.Info("--------------------My application starts with Chrome Extension message: " + chromeMessage + "---------------------------------");
+            }
+        }
+
+        private static string OpenStandardStreamIn()
+        {
+            Stream stdin = Console.OpenStandardInput();
+            int length = 0;
+            byte[] bytes = new byte[4];
+            stdin.Read(bytes, 0, 4);
+            length = System.BitConverter.ToInt32(bytes, 0);
+
+            string input = "";
+            for (int i = 0; i < length; i++)
+            {
+                input += (char)stdin.ReadByte();
+            }
+
+            return input;
+        }
+    }
+}
---
+++ ChromeExtensionDemo/MyApp/Program.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using System;
+using System.IO;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            JObject data;
+            while ((data = Read()) != null)
+            {
+                var processed = ProcessMessage(data);
+                Write(processed);
+                if (processed == "exit")
+                {
+                    return;
+                }
+            }
+        }
+
+        public static string ProcessMessage(JObject data)
+        {
+            var message = data["text"].Value<string>();
+            log2file(message);
+            switch (message)
+            {
+                case "test":
+                    return "testing!";
+
+                case "exit":
+                    return "exit";
+
+                default:
+                    return "echo: " + message;
+            }
+        }
+
+        public static JObject Read()
+        {
+            var stdin = Console.OpenStandardInput();
+            var length = 0;
+
+            var lengthBytes = new byte[4];
+            stdin.Read(lengthBytes, 0, 4);
+            length = BitConverter.ToInt32(lengthBytes, 0);
+
+            var buffer = new char[length];
+            using (var reader = new StreamReader(stdin))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    reader.Read(buffer, 0, buffer.Length);
+                }
+            }
+
+            return (JObject)JsonConvert.DeserializeObject<JObject>(new string(buffer));
+        }
+
+        public static void Write(JToken data)
+        {
+            var json = new JObject();
+            json["data"] = data;
+
+            var bytes = System.Text.Encoding.UTF8.GetBytes(json.ToString(Formatting.None));
+
+            var stdout = Console.OpenStandardOutput();
+            stdout.WriteByte((byte)((bytes.Length >> 0) & 0xFF));
+            stdout.WriteByte((byte)((bytes.Length >> 8) & 0xFF));
+            stdout.WriteByte((byte)((bytes.Length >> 16) & 0xFF));
+            stdout.WriteByte((byte)((bytes.Length >> 24) & 0xFF));
+            stdout.Write(bytes, 0, bytes.Length);
+            stdout.Flush();
+        }
+
+
+        private static void log2file(string s)
+        {
+            FileStream fs = new FileStream(@"c:\test1.log", FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(s);
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
