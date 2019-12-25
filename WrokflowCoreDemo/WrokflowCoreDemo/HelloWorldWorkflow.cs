5ded282590c439b589232baa2e59309772bb5b84
wturi <bolizhazi@live.cn>
2019-12-25 10:19:52 +0800

    update

---
+++ WrokflowCoreDemo/WrokflowCoreDemo/HelloWorldWorkflow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkflowCore.Interface;
+
+namespace WrokflowCoreDemo
+{
+    public class HelloWorldWorkflow : IWorkflow
+    {
+        public string Id => "Hello World";
+
+        public int Version => 1;
+
+        public void Build(IWorkflowBuilder<object> builder)
+        {
+            builder
+                .StartWith<HelloWorld>()
+                .Then<ActiveWorld>()
+                .Then<GoodbyeWorld>();
+        }
+    }
+}
---
+++ WrokflowCoreDemo/WrokflowCoreDemo/Steps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace WrokflowCoreDemo
+{
+    public class Steps
+    {
+    }
+
+    public class HelloWorld : StepBody
+    {
+        public override ExecutionResult Run(IStepExecutionContext context)
+        {
+            Console.WriteLine("Hello World");
+            return ExecutionResult.Next();
+        }
+    }
+    public class ActiveWorld : StepBody
+    {
+        public override ExecutionResult Run(IStepExecutionContext context)
+        {
+            Console.WriteLine("I am activing in the World!");
+            return ExecutionResult.Next();
+        }
+    }
+
+    public class GoodbyeWorld : StepBody
+    {
+        public override ExecutionResult Run(IStepExecutionContext context)
+        {
+            Console.WriteLine("Goodbye World!");
+            return ExecutionResult.Next();
+        }
+    }
+}
