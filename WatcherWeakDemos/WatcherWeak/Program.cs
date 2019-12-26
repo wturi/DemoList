1b8442b7a9fa4444633784e369bc4790f83e08aa
wturi <bolizhazi@live.cn>
2019-12-26 13:29:20 +0800

    update

---
+++ WatcherWeakDemos/WatcherWeak/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace WatcherWeak
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            _wather = new FileSystemWatcher(@"D:\11.txt")
+            {
+                EnableRaisingEvents = true
+            };
+
+            var weakEvent = new FileSystemWatcherWeakEventRelay(_wather);
+            weakEvent.Created += OnCreated;
+            weakEvent.Changed += OnChanged;
+            weakEvent.Renamed += OnRenamed;
+            weakEvent.Deleted += OnDeleted;
+        }
+
+        private static FileSystemWatcher _wather;
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("OnCreated");
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("OnChanged");
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine("OnRenamed");
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("OnDeleted");
+        }
+    }
+}
