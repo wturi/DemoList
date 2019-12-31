6ea939cd69e6929b55cb6db495b7a400fdd604c6
wturi <bolizhazi@live.cn>
2019-12-31 18:28:29 +0800

    添加 C#反射遍历对象属性 DEMO

---
+++ ReflectDemos/ReflectDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectDemo
+{
+    /// <summary>
+    /// C#反射遍历对象属性
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("hh:mm:ss-ffff"));
+            AddressInfo model = new AddressInfo();
+            ForeachClassProperties(model); 
+            Console.WriteLine(DateTime.Now.ToString("hh:mm:ss-ffff"));
+            Console.ReadLine();
+        }
+
+        private static void ForeachClassProperties<T>(T model)
+        {
+            Type t = model.GetType();
+            PropertyInfo[] PropertyList = t.GetProperties();
+            foreach (PropertyInfo item in PropertyList)
+            {
+                string name = item.Name;
+                object value = item.GetValue(model, null);
+            }
+        }
+    }
+
+    public class AddressInfo
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserTel { get; set; }
+        public string Addressdetail { get; set; }
+        public int IsMoren { get; set; }
+
+        public AddressInfo()
+        {
+            Id = 1;
+            UserName = "陈卧龙";
+            UserTel = "1813707015*";
+            Addressdetail = "江苏省苏州市工业园区国际科技园";
+            IsMoren = 1;
+        }
+    }
+}
--- SharedMemory/ReadApp/Program.cs
+++ SharedMemory/ReadApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO.MemoryMappedFiles;
+using System.Text;
 using System.Threading;
 
 namespace ReadApp
@@ -10,23 +11,32 @@ namespace ReadApp
         {
             long capacity = 1 << 10 << 10;
 
-            using (var mmf = MemoryMappedFile.OpenExisting("BotTimeNativeMessageHostSharedMemory"))
+            try
             {
-                MemoryMappedViewAccessor viewAccessor = mmf.CreateViewAccessor(0, capacity);
-
-                //循环刷新共享内存字符串的值
-                while (true)
+                using (var mmf = MemoryMappedFile.OpenExisting("BotTimeNativeMessageHostSharedMemory"))
                 {
-                    //读取字符长度
-                    int strLength = viewAccessor.ReadInt32(0);
-                    char[] charsInMMf = new char[strLength];
-                    //读取字符
-                    viewAccessor.ReadArray<char>(4, charsInMMf, 0, strLength);
-                    Console.Clear();
-                    Console.Write(charsInMMf);
-                    Console.Write("\r");
-                    Thread.Sleep(200);
+                    MemoryMappedViewAccessor viewAccessor = mmf.CreateViewAccessor(0, capacity);
+
+                    //循环刷新共享内存字符串的值
+                    while (true)
+                    {
+                        //读取字符长度
+                        int strLength = viewAccessor.ReadInt32(0);
+                        char[] charsInMMf = new char[strLength];
+                        //读取字符
+                        viewAccessor.ReadArray<char>(4, charsInMMf, 0, strLength);
+                        Console.Clear();
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(charsInMMf);
+                        Console.Write(sb.ToString());
+                        Console.Write("\r");
+                        Thread.Sleep(200);
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                throw;
             }
         }
     }
