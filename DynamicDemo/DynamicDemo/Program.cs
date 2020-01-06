2ec44f9a512d3ae1ddd3a825be2e386bdfcec9ee
wturi <bolizhazi@live.cn>
2020-01-06 20:10:19 +0800

    update

--- DynamicDemo/DynamicDemo/Program.cs
+++ DynamicDemo/DynamicDemo/Program.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace DynamicDemo
 {
     internal class Program
     {
         private static void Main(string[] args)
+        {
+
+            var returnObj = GetDynamicClassBydt();
+            Console.WriteLine(JsonConvert.SerializeObject(returnObj));
+
+            Console.ReadLine();
+        }
+
+
+        private static void Run1()
         {
             dynamic foo = new DemoMethod();
             //可以直接调用方法
@@ -15,6 +26,19 @@ namespace DynamicDemo
 
             Console.WriteLine(msg);
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 使用dynamic根据DataTable的列名自动添加属性并赋值
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns> 
+        private static Object GetDynamicClassBydt()
+        {
+            dynamic d = new System.Dynamic.ExpandoObject();
+            (d as System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<string, object>>).Add(new System.Collections.Generic.KeyValuePair<string, object>("numA", 1));
+
+            return d;
         }
     }
 }
