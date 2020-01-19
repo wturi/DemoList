87c0f7e8d2cb73166af024fc12127a5ae19f6c4f
wturi <bolizhazi@live.cn>
2020-01-19 10:30:41 +0800

    递归累加验证

---
+++ Demos/Recursive/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Recursive
+{
+    /// <summary>
+    /// 递归
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Add();
+            Console.ReadLine();
+        }
+
+        private static void Add(int num = 0)
+        {
+            Console.WriteLine(num);
+            if (num < 10) Add(++num);
+        }
+    }
+}
