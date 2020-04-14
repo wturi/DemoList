ac7303e3adc6ce7b2bdfb5cf78570bcafaf3b9ab
wturi <bolizhazi@live.cn>
2020-04-14 17:41:38 +0800

    添加 获取父进程 demo

---
+++ Demos/GetParentProcess/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetParentProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parent(Process.GetCurrentProcess(), new[] { "cmd" });
+
+            Console.ReadLine();
+        }
+
+
+
+
+        /// <summary>
+        /// 获取进程的父进程
+        /// </summary>
+        /// <param name="process">进程</param>
+        /// <param name="ignoreName">需要忽略的父进程名称，如果有则忽略这一层，继续网上找</param>
+        /// <returns></returns>
+        private static Process Parent(Process process, string[] ignoreName)
+        {
+            try
+            {
+                var parentProcess = FindPidFromIndexedProcessName(FindIndexedProcessName(process.Id));
+                if (parentProcess == null) return null;
+                if (ignoreName == null) return parentProcess;
+                return ignoreName.Contains(parentProcess.ProcessName) ? Parent(parentProcess, ignoreName) : parentProcess;
+            }
+            catch (Exception e)
+            {
+                //如果没有找到则返回null
+                return null;
+            }
+        }
+
+        private static string FindIndexedProcessName(int pid)
+        {
+            var processName = Process.GetProcessById(pid).ProcessName;
+            var processesByName = Process.GetProcessesByName(processName);
+            string processIndexedName = null;
+            for (var index = 0; index < processesByName.Length; index++)
+            {
+                processIndexedName = index == 0 ? processName : processName + "#" + index;
+                var processId = new PerformanceCounter("Process", "ID Process", processIndexedName);
+                if ((int)processId.NextValue() == pid)
+                {
+                    return processIndexedName;
+                }
+            }
+            return processIndexedName;
+        }
+
+        private static Process FindPidFromIndexedProcessName(string indexedProcessName)
+        {
+            var parentId = new PerformanceCounter("Process", "Creating Process ID", indexedProcessName);
+            return Process.GetProcessById((int)parentId.NextValue());
+        }
+    }
+}
