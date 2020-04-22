bce06da9cf1070c3b4e6311092452889fadf5967
wturi <bolizhazi@live.cn>
2020-04-22 20:31:55 +0800

    update

--- Process/GetProcessInfo/MainWindow.xaml.cs
+++ Process/GetProcessInfo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 
@@ -18,55 +19,61 @@ namespace GetProcessInfo
 
         private void Run_OnClick(object sender, RoutedEventArgs e)
         {
-            var resultStr = string.Empty;
-
-            var pidStr = Pid.Text;
-
-            if (!int.TryParse(pidStr, out var pid))
+            try
             {
-                MessageBox.Show("请输入Pid");
-                return;
-            }
+                var pidStr = Pid.Text;
 
-            var process = Process.GetProcessById(pid);
-
-            if (BrotherProcess.IsChecked != null && (bool)BrotherProcess.IsChecked)
-            {
-                //兄弟进程
-                var parentProcess = Helpers.ProcessHelper.Parent(process, null);
-                var processList = Helpers.ProcessHelper.GetChildProcesses(parentProcess.Id);
-                var result = processList.Select(p => new
+                if (!int.TryParse(pidStr, out var pid))
                 {
-                    p.ProcessName,
-                    ProcessId = p.Id
-                });
+                    MessageBox.Show("请输入Pid");
+                    return;
+                }
 
-                ShowTextBox.Text = JsonConvert.SerializeObject(result);
-            }
-            else if (ChildrenProcess.IsChecked != null && (bool)ChildrenProcess.IsChecked)
-            {
-                //子进程
-                var processList = Helpers.ProcessHelper.GetChildProcesses(process.Id);
-                var result = processList.Select(p => new
+                var process = Process.GetProcessById(pid);
+
+                if (BrotherProcess.IsChecked != null && (bool)BrotherProcess.IsChecked)
                 {
-                    p.ProcessName,
-                    ProcessId = p.Id
-                });
+                    //兄弟进程
+                    var parentProcess = Helpers.ProcessHelper.Parent(process, null);
+                    var processList = Helpers.ProcessHelper.GetChildProcesses(parentProcess.Id);
+                    var result = processList.Select(p => new
+                    {
+                        p.ProcessName,
+                        ProcessId = p.Id
+                    });
 
-                ShowTextBox.Text = JsonConvert.SerializeObject(result);
-            }
-            else
-            {
-                //父进程
-                var parentProcess = Helpers.ProcessHelper.Parent(process, null);
-                var result = new
+                    ShowTextBox.Text = JsonConvert.SerializeObject(result);
+                }
+                else if (ChildrenProcess.IsChecked != null && (bool)ChildrenProcess.IsChecked)
                 {
-                    parentProcess?.ProcessName,
-                    ProcessId = parentProcess?.Id
-                };
+                    //子进程
+                    var processList = Helpers.ProcessHelper.GetChildProcesses(process.Id);
+                    var result = processList.Select(p => new
+                    {
+                        p.ProcessName,
+                        ProcessId = p.Id
+                    });
 
-                ShowTextBox.Text = JsonConvert.SerializeObject(result);
+                    ShowTextBox.Text = JsonConvert.SerializeObject(result);
+                }
+                else
+                {
+                    //父进程
+                    var parentProcess = Helpers.ProcessHelper.Parent(process, null);
+                    var result = new
+                    {
+                        parentProcess?.ProcessName,
+                        ProcessId = parentProcess?.Id
+                    };
+
+                    ShowTextBox.Text = JsonConvert.SerializeObject(result);
+                }
             }
+            catch (Exception exception)
+            {
+                ShowTextBox.Text = JsonConvert.SerializeObject(exception);
+            }
+
         }
     }
 }
