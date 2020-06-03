8fc1af6fe1d96b894367744ca0d0f02abf5a6d49
wturi <bolizhazi@live.cn>
2020-06-03 20:24:13 +0800

    update

---
+++ DemoSample/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = Process.GetProcessesByName("Encoo.Executor").FirstOrDefault();
+
+            var counter = new PerformanceCounter("Process", "Working Set - Private", p.ProcessName);
+
+            var raw = counter.RawValue / 1024;
+        }
+    }
+}
