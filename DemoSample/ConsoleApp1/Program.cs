485ba8a3bf4f5f5c1a2725f6102054210bd7ec24
wturi <bolizhazi@live.cn>
2020-06-03 20:52:51 +0800

    update

--- DemoSample/ConsoleApp1/Program.cs
+++ DemoSample/ConsoleApp1/Program.cs
@@ -1,40 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            long firstNum = 0;
+
+            long lastNum = 0;
+
             while (true)
             {
                 try
                 {
-
                     var p = Process.GetProcessesByName("Encoo.Executor").FirstOrDefault();
 
                     var counter = new PerformanceCounter("Process", "Working Set - Private", p.ProcessName);
 
                     var raw = counter.RawValue / 1024;
 
-                    using (var file = new System.IO.StreamWriter(@"C:\BVTResult\raw.txt", true))
+                    if (firstNum == 0)
                     {
-                        file.WriteLine(raw.ToString());// 直接追加文件末尾，换行 
+                        firstNum = raw;
                     }
 
-                    Console.WriteLine(raw);
+                    Console.WriteLine($"{raw}   {raw - firstNum}   {raw - lastNum}");
+
+                    lastNum = raw;
+
+                    using (var file = new System.IO.StreamWriter(@"C:\BVTResult\raw.txt", true))
+                    {
+                        file.WriteLine(raw.ToString());// 直接追加文件末尾，换行
+                    }
 
                     Thread.Sleep(5000);
                 }
                 catch (Exception)
                 {
-
                 }
             }
         }
