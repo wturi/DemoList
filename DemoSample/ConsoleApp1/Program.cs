10d8369aedf026a605df302a0895819812f13861
wturi <bolizhazi@live.cn>
2020-06-24 18:25:57 +0800

    update

--- DemoSample/ConsoleApp1/Program.cs
+++ DemoSample/ConsoleApp1/Program.cs
@@ -8,28 +8,25 @@ namespace ConsoleApp1
     {
         private static void Main(string[] args)
         {
-            var processName = "";
+            var onclick = "ContainerIno1(25429091,1003052,1003051,\"\",5)";
 
-            Console.WriteLine("输入process name(默认为：EncooNativeMessageHost)：");
-            processName = Console.ReadLine();
+            var leftParenthesis = onclick.IndexOf("(", StringComparison.Ordinal) + 1;
 
-            if (string.IsNullOrEmpty(processName))
-            {
-                processName = "EncooNativeMessageHost";
-            }
+            var comma1 = onclick.IndexOf(",", StringComparison.Ordinal);
 
-            while (true)
-            {
-                try
-                {
-                    var ps = Process.GetProcessesByName(processName);
-                    Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}  process num :{ps.Length}");
-                    Thread.Sleep(200);
-                }
-                catch (Exception)
-                {
-                }
-            }
+            var comma2 = onclick.IndexOf(',', comma1 + 1);
+
+            var comma3 = onclick.IndexOf(',', comma2 + 1);
+
+            var cntId = onclick.Substring(leftParenthesis, comma1 - leftParenthesis);
+
+            var terId = onclick.Substring(comma1 + 1, comma2 - comma1 - 1);
+
+            var iId = onclick.Substring(comma2 + 1, comma3 - comma2 - 1);
+
+            Console.WriteLine(cntId);
+
+            Console.ReadLine();
         }
     }
 }
