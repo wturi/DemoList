3d660a04cd117e63c28db59fe0e16da48ece48f3
wturi <bolizhazi@live.cn>
2020-06-30 20:13:51 +0800

    update

---
+++ DemoSample/BrowserCommandMQ/BrowserCOmmandMqCmdDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserCOmmandMqCmdDemo
+{
+    class Program
+    {
+        public enum ShowCommands : int
+        {
+            SW_HIDE = 0,
+            SW_SHOWNORMAL = 1,
+            SW_NORMAL = 1,
+            SW_SHOWMINIMIZED = 2,
+            SW_SHOWMAXIMIZED = 3,
+            SW_MAXIMIZE = 3,
+            SW_SHOWNOACTIVATE = 4,
+            SW_SHOW = 5,
+            SW_MINIMIZE = 6,
+            SW_SHOWMINNOACTIVE = 7,
+            SW_SHOWNA = 8,
+            SW_RESTORE = 9,
+            SW_SHOWDEFAULT = 10,
+            SW_FORCEMINIMIZE = 11,
+            SW_MAX = 11
+        }
+        [DllImport("shell32.dll")]
+        static extern IntPtr ShellExecute(
+            IntPtr hwnd,
+            string lpOperation,
+            string lpFile,
+            string lpParameters,
+            string lpDirectory,
+            ShowCommands nShowCmd);
+
+        static void Main(string[] args)
+        {
+            ShellExecute(IntPtr.Zero, "open", @"c:\test.flv", "", "", ShowCommands.SW_SHOWNORMAL);
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ DemoSample/Demos/BrowserCommandMQ/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+using Fleck;
+
+namespace BrowserCommandMQ
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SingletonRun();
+
+            WebSocketServerStart(args);
+
+            while (true)
+            {
+                
+            }
+        }
+
+        /// <summary>
+        /// 执行
+        /// </summary>
+        /// <param name="args"></param>
+        private static void WebSocketServerStart(string[] args)
+        {
+            var port = 8021;
+            var wsServer = new WebSocketServer($"ws://127.0.0.1:{port}");
+            var clientManager = new ClientConnectionManager();
+            wsServer.Start(connection =>
+            {
+                var conn = connection.ConnectionInfo;
+                connection.OnOpen = () => clientManager.AddConnection(conn.ClientPort, connection);
+                connection.OnMessage = clientMessage => { };
+                connection.OnError = error => { clientManager.RemoveConnection(conn.ClientPort); };
+                connection.OnClose = () => clientManager.RemoveConnection(conn.ClientPort);
+            });
+        }
+
+        /// <summary>
+        /// 单例运行
+        /// </summary>
+        private static void SingletonRun()
+        {
+            var mutex = new Mutex(true, System.Diagnostics.Process.GetCurrentProcess().ProcessName, out var isAppRunning);
+
+            if (!isAppRunning)
+            {
+                Environment.Exit(1);
+            }
+        }
+    }
+}
