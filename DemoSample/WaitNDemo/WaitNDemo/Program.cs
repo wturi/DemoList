fe3194e3e9ec5bcbd0c4c95044237fc1b38692c8
wturi <bolizhazi@live.cn>
2020-08-25 19:12:28 +0800

    添加 waitN demo

---
+++ DemoSample/WaitNDemo/WaitNDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using WatiN.Core;
+
+namespace WaitNDemo
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            using (var ie = Browser.AttachTo<IE>(Find.ByTitle("百度一下，你就知"), 2400))
+            {
+                ie.TextField(Find.ById("kw")).Focus();
+
+                var element = ie.ActiveElement;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
