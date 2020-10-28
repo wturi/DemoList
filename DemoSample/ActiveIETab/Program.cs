f3a4c91aed4ae4c8b4f32beff2e759cf87cd30b1
wturi <bolizhazi@live.cn>
2020-10-28 16:28:00 +0800

    update
    update

---
+++ DemoSample/ActiveIETab/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using SHDocVw;
+
+namespace ActiveIETab
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var url = "https://encootest.chinacloudsites.cn/";
+
+            ShellWindows shellWindows = new SHDocVw.ShellWindows();
+
+            string filename;
+
+            foreach (SHDocVw.InternetExplorer ie in shellWindows)
+            {
+                filename = System.IO.Path.GetFileNameWithoutExtension(ie.FullName).ToLower();
+
+                if (filename.Equals("iexplore") && ie.LocationURL.Contains(url))
+
+                {
+                    new TabActivator((IntPtr)ie.HWND).ActivateByTabsUrl(ie.LocationURL);
+
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ DemoSample/ConsoleApp3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+
+    }
+
+    public class ListNode
+    {
+        public int Val;
+        public ListNode Next;
+        public ListNode(int x) { Val = x; }
+    }
+
+    public class Solution
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null) return true;
+            var nodeList = new List<int>();
+
+            while (head != null)
+            {
+                nodeList.Add(head.Val);
+                head = head.Next;
+            }
+
+
+            for (int i = 0, j = nodeList.Count - 1; i < j; i++, j--)
+            {
+                if (nodeList[i] != nodeList[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
